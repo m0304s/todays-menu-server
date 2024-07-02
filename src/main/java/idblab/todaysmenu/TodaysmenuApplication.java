9d7b93d8eb95c358e643f13481ace17feb20a2ae
m0304s <cms8955@naver.com>
2024-07-02 15:52:56 +0900

    Initialize Setting

---
+++ src/main/java/idblab/todaysmenu/TodaysmenuApplication.java
@@ -0,0 +1,13 @@
+package idblab.todaysmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaysmenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodaysmenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idblab/todaysmenu/TodaysmenuApplicationTests.java
@@ -0,0 +1,13 @@
+package idblab.todaysmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaysmenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
